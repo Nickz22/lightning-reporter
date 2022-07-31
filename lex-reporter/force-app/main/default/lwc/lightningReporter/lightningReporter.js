429c1e6333f96939cc50d38eb9e0bcf903be24c2
Nicholas Zozaya <nickzsb.nz@gmail.com>
2022-07-31 11:35:06 -0700

    working controller and fe skeleton

--- lex-reporter/force-app/main/default/lwc/lightningReporter/lightningReporter.js
+++ lex-reporter/force-app/main/default/lwc/lightningReporter/lightningReporter.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import getChildTypes from '@salesforce/apex/LightningReporterController.getChildTypes';
+import getRecordsFromType from '@salesforce/apex/LightningReporterController.getRecordsFromType'
 
-export default class LightningReporter extends LightningElement {}
+export default class LightningReporter extends LightningElement {
+    @api recordId;
+    @track childRecords;
+    childTypes;
+    @wire (getChildTypes, {recordId : '$recordId'})
+    getRecordsFromDefaultChildType({error, data}){
+        if(error){
+            console.error('error getting default child records: '+error)
+            return;
+        }
+        if(data){
+            this.childTypes = data;
+            console.log('retrieved '+this.childTypes.length+' child types: ');
+            this.getDefaultChildRecords(this.recordId);
+        }else{
+            console.error('no data returned from getChildTypes');
+        }
+    }
+
+    getDefaultChildRecords(){
+        console.log('getting records from '+this.childTypes[0]+' type with Id '+this.recordId);
+        getRecordsFromType({
+            typeName: this.childTypes[0],
+            parentId: this.recordId
+        }).then(result => {
+            console.log('retrieved '+result.length+' records from type: '+this.childTypes[0]);
+            this.childRecords = result;
+        }).catch(error => {
+            console.error('error getting records ['+error.body.message+']');
+        })
+    }
+}
