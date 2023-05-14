4cb4a01cdafcb3d711ac1baf7582dae455afe0bb
Nicholas Zozaya <nickzsb.nz@gmail.com>
2023-05-14 15:44:35 -0700

    some improvements

--- lex-reporter/force-app/main/default/lwc/table/table.js
+++ lex-reporter/force-app/main/default/lwc/table/table.js
@@ -6,13 +6,6 @@ export default class Table extends LightningElement {
   @api saved;
   @api topMostId;
 
-  connectedCallback() {
-    console.log(`table.js: connectedCallback: fields: ${this.fields}`);
-    console.log(`table.js: connectedCallback: records: ${this.records}`);
-    console.log(`table.js: connectedCallback: saved: ${this.saved}`);
-    console.log(`table.js: connectedCallback: topMostId: ${this.topMostId}`);
-  }
-
   @api getRows() {
     return this.template.querySelectorAll("c-table-row");
   }
--- lex-reporter/force-app/main/default/lwc/tableRow/tableRow.js
+++ lex-reporter/force-app/main/default/lwc/tableRow/tableRow.js
@@ -267,14 +267,8 @@ export default class TableRow extends NavigationMixin(LightningElement) {
       this.previewAvatar.unreadStyle = "";
       if (this.notes.length > 0) {
         this.notes = [];
-        // set table row class to table-row-expanded
-        // let tableRow = this.template.querySelector(".table-row");
-        // tableRow.classList.remove("table-row-expanded");
       } else {
         this.notes = this.avatars;
-        // set table row class to table-row-expanded
-        // let tableRow = this.template.querySelector(".table-row");
-        // tableRow.classList.add("table-row-expanded");
       }
     } catch (error) {
       this.showNotification("Error", error.message, "error");
